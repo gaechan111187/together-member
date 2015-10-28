9f0b04f6d1eaa2d7c931b52ccc99ac76715c8492
HB <HB@HB-PC>
2015-10-28 12:53:00 +0900

    2015.10.28 12시 52

--- together-member/src/member/JoinUsUI.java
+++ together-member/src/member/JoinUsUI.java
@@ -1,6 +1,8 @@
 package member;
 
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -23,7 +25,9 @@ public class JoinUsUI extends JFrame implements ActionListener {
 		super("회원가입");
 		this.client = client;
 		jPanel = new JPanel();
-		jPanel.setLayout(new GridLayout(9, 1));
+		jPanel.setLayout(new GridLayout(10, 1));
+		jPanel.setBackground(new Color(159, 197, 248)); //변경된 부분
+		
 		b1 = new JButton("회원가입");
 		
 		jPanel.add(new JLabel("  이름     ")); //이름 라벨 붙임.
@@ -42,6 +46,9 @@ public class JoinUsUI extends JFrame implements ActionListener {
 		jPanel.add(new JLabel("   이메일 "));
 		fieldEmail = new JTextField(20);
 		jPanel.add(fieldEmail);
+		
+		jPanel.add(new Label("     ")); // 변경
+		
 		jPanel.add(b1);
 		b1.addActionListener(this);
 		add(jPanel);
